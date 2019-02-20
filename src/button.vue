b8c9aa214901ff6b25536a3947ea9e3d71044d5d
xiakai <xiakai@reeli.cn>
2019-02-20 14:43:08 +0800

    验证 iconPosition 为 left 或 right 中的一个

--- src/button.vue
+++ src/button.vue
@@ -9,7 +9,17 @@
 
 <script>
 export default {
-	props: ['icon', 'iconPosition']
+	props: {
+		icon: {},
+		iconPosition: {
+			type: String,
+			default: 'left',
+			// 属性检查器
+			validator(value) {
+				return value === 'left' || value === 'right'
+			}
+		}
+	}
 }
 </script>
 
