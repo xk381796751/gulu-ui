8a9572fcca29adbcbad0d172e912322c20137b84
xiakai <xiakai@reeli.cn>
2019-02-26 17:59:59 +0800

    让 input 支持 v-model 双向绑定

--- src/app.js
+++ src/app.js
@@ -15,15 +15,11 @@ new Vue({
 	data: {
 		loading1: false,
 		loading2: true,
-		loading3: false
+		loading3: false,
+		message: 'hello'
 	},
 	created() {
-		setTimeout(() => {
-			let event = new Event('change');
-			let inputElement = this.$el.querySelector('input')
-			inputElement.dispatchEvent(event)
-			console.log('hi')
-		}, 3000)
+		
 	},
 	methods: {
 		inputChange(e) {
